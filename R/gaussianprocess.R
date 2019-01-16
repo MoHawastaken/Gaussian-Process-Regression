107b6eb281a05ff8279e44ce3819abbd38d1f002
MBudjan <budjan@stud.uni-heidelberg.de>
2019-01-16 10:50:19 +0100

    added package structure

---
+++ R/gaussianprocess.R
@@ -0,0 +1,6 @@
+#' Gaussian Process Regression
+#' 
+#' @details
+#' gaussianprocess implements regression an classification functions for a Gaussian Process
+#' @keywords internal
+"_PACKAGE"
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(gaussianprocess)
+
+test_check("gaussian-process-regression")
