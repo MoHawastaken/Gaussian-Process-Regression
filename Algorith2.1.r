c6b52eb577a782ceb028996c6cf3185587a99b4b
MBudjan <budjan@stud.uni-heidelberg.de>
2019-01-16 10:11:35 +0100

    corrected t() corrected style errors

--- Algorith2.1.r
+++ Algorith2.1.r
@@ -1,33 +1,33 @@
 #Algorithm 2.1
 
-regression<- function(X,y,k,sigma,Xs){
+regression <- function(X,y,k,sigma,Xs){
   
   #Calculate K(X,X)
-  VielX <- matrix(NA,nrow=length(X),ncol=length(X))
-  for(i in 1:length(X)){
-    Xi<-matrix(X[i],nrow=length(X),ncol=1) 
+  VielX <- matrix(NA,nrow = length(X), ncol = length(X))
+  for (i in 1:length(X)) {
+    Xi <- matrix(X[i],nrow = length(X), ncol = 1) 
     VielX[,i] <- Xi 
   }
-  K <- matrix(mapply(k,VielX,X),nrow=length(X),byrow=TRUE)
+  K <- matrix(mapply(k,VielX,X),nrow = length(X), byrow = TRUE)
   #K calculated
   
   #Calculate k* (ks)
-  XS<-rep(Xs,length(X))
+  XS <- rep(Xs,length(X))
   ks <- mapply(k,X,XS)
   #end
   
   #calculate all other variables directly
-  L <- chol(K+sigma*diag(length(X)))
+  L <- chol(K + sigma*diag(length(X)))
   
-  alpha<-solve(L^T,solve(L,y))
+  alpha <- solve(t(L),solve(L,y))
   
-  fs <- ks^T%*%alpha
+  fs <- t(ks) %*% alpha
   
   v <- solve(L,ks)
   
-  Vfs <- k(xs,xs)-v^T%*%v
+  Vfs <- k(Xs,Xs) - v^T %*% v
   
-  logp <- -0.5*y^T%*%alpha-sum(log(diag(L)))-length(X)/2*log(2*pi)
+  logp <- -0.5*t(y) %*% alpha - sum(log(diag(L))) - length(X)/2*log(2*pi)
   
   return(c(fs,Vfs,logp))
 }  
