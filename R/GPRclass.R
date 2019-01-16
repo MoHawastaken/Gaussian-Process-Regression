c93668bd5eca532698c2c103d1b4b9f1b44aac33
Konstantin K <konstantin.j.klumpp@gmail.com>
2019-01-16 12:19:21 +0100

    Added .L, .alpha and their computation to GPR class.

--- R/GPRclass.R
+++ R/GPRclass.R
@@ -5,12 +5,25 @@ GPR <- R6::R6Class("GPR",
                      .X = NA,
                      .k = NA,
                      .y = NA,
+                     .L = NA,
+                     .alpha = NA,
                      .noise = NA
                    ),
                    public = list(
                      initialize = function(X, y, k, noise){
                        private$.X <-  X
                        private$.k <-  k
+                       #K <- outer(1:ncol(X), 1:ncol(X), function(i,j) k(X[, factor(i)], X[, factor(j)]))
+                       n = ncol(X)
+                       K <- matrix(0, nrow = n, ncol = n)
+                       for (i in 1:n) {
+                         for (j in 1:n) {
+                           K[i, j] = k(X[, i], X[, j])
+                         }
+                       }
+                       private.L <- chol(K + sigma * diag(n))
+                       
+                       private.alpha <- solve(t(L), solve(L, y))
                        private$.y <- y
                        private$.noise <- noise
                      }
