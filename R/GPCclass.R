5c295a075ab3047986bed83873699b1746924d76
Konstantin K <konstantin.j.klumpp@gmail.com>
2019-01-23 16:11:49 +0100

    Adapted plotting method for classification

--- R/GPCclass.R
+++ R/GPCclass.R
@@ -11,7 +11,7 @@ GPC <- R6::R6Class("GPR",
                    ),
                    public = list(
                      initialize = function(X, y, k, epsilon){
-                       stopifnot(is.matrix(X), is.vector(y), is.numeric(y))
+                       stopifnot(is.matrix(X), is.vector(y), is.numeric(y), length(y) == ncol(X))
                        stopifnot(is.numeric(epsilon), epsilon > 0, is.function(k))
                        n <- length(y)
                        K <- matrix(0, nrow = n, ncol = n)
@@ -35,7 +35,7 @@ GPC <- R6::R6Class("GPR",
                          f <- c(K %*% a)
                          print(a)
                          print(f)
-                         objective <- -sum(a * f)/2 + sum(log(1/(1 + exp(-y * f))))
+                         objective <- -sum(a * f)/2 - sum(log(1 + exp(-y * f)))
                          print(objective)
                          if (it > 1) {
                            if (abs(objective - last_objective) < epsilon) {
@@ -53,6 +53,7 @@ GPC <- R6::R6Class("GPR",
                        W <- (1 - Pi)*Pi
                        private$.f_hat <- f
                        private$.L <- t(chol(diag(n) + (sqrt(W) %o% sqrt(W)) * K))
+                       private$.logq <- objective - sum(diag(self$L))
                        private$.X <- X
                        private$.y <- y
                        private$.k <- k
@@ -62,21 +63,21 @@ GPC <- R6::R6Class("GPR",
                        W <- Pi*(1 - Pi)
                        ks <- sapply(1:ncol(self$X), FUN = function(i) self$k(self$X[, i], Xs))
                        fs_bar <- sum(ks * ((self$y + 1)/2 - Pi))
-                       v <- solve(self$L, (sqrt(W)*ks))
+                       v <- solve(self$L, (sqrt(W) * ks))
                        Vfs <- self$k(Xs, Xs) - sum(v * v)
-                       hilfs_func <- function(z) 1/(1+exp(-z))*(1/sqrt(2*pi*Vfs))*exp(-(z-fs_bar)^2/(2*Vfs))
+                       hilfs_func <- function(z) 1/(1 + exp(-z))  * (1/sqrt(2*pi*Vfs)) * exp(-(z-fs_bar)^2/(2*Vfs))
                        PIs_hat <- integrate(hilfs_func, -Inf, Inf)[1]
-                       return(PIs_hat)
+                       return(PIs_hat$value)
                      },
                      plot = function(testpoints){
                        dat <- data.frame(x = testpoints, 
-                                         y = t(sapply(testpoints, function(x) self$predict(x)[1:2])))
-                       ggplot2::ggplot(dat, ggplot2::aes(x = x, y = y.1)) +
+                                         y = sapply(testpoints, function(x) self$predict_class(x)))
+                       ggplot2::ggplot(dat, ggplot2::aes(x = x, y = y)) +
                          ggplot2::theme_classic() +
-                         ggplot2::scale_y_continuous("output, f(x)") +
+                         ggplot2::scale_y_continuous("output, p(y = 1| x)") +
                          ggplot2::geom_line() +
-                         ggplot2::geom_ribbon(ggplot2::aes(ymin = y.1 - 2*sqrt(max(y.2,0)),
-                                                           ymax = y.1 + 2*sqrt(max(y.2,0))), alpha = 0.2) +
+                         #ggplot2::geom_ribbon(ggplot2::aes(ymin = y.1 - 2*sqrt(max(y.2,0)),
+                         #                                  ymax = y.1 + 2*sqrt(max(y.2,0))), alpha = 0.2) +
                          ggplot2::geom_point(data = data.frame(xpoints = c(self$X), ypoints = self$y), 
                                              mapping = ggplot2::aes(x = xpoints, y = ypoints, shape = 4)) +
                          ggplot2::scale_shape_identity()
@@ -128,7 +129,14 @@ GPC <- R6::R6Class("GPR",
                    )
 )
 
-X <- matrix(seq(-1,1,by = 0.2), nrow = 1)
+X <- matrix(seq(-1,1,by = 0.1), nrow = 1)
 y <- as.integer(X > 0)
-kappa <- function(x,y) exp(-(x - y)^2)
-gaussian_classifier <- GPC$new(X, y, kappa, 0.1)
+kappa <- function(x,y) exp(-3*(x - y)^2)
+gaussian_classifier <- GPC$new(X, y, kappa, 1e-5)
+gaussian_classifier$plot(seq(-2,2, by = 0.1))
+
+X <- matrix(c(seq(-1, -0.1, by = 0.1), seq(0, 1, by = 0.2)), nrow = 1)
+y <- as.integer(X > 0)
+kappa <- function(x,y) exp(-3*(x - y)^2)
+gaussian_classifier <- GPC$new(X, y, kappa, 1e-5)
+gaussian_classifier$plot(seq(-2,2, by = 0.1))
