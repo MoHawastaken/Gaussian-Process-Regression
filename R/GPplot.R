e5a4eb23f0b2de8f5d2918c0bee7e558066ed80e
MBudjan <budjan@stud.uni-heidelberg.de>
2019-01-24 22:24:55 +0100

    added features to shiny app

--- R/GPplot.R
+++ R/GPplot.R
@@ -1,35 +1,148 @@
 library(shiny)
 
 ui <- fluidPage(
-  selectInput("cov", "Covariance Function", choices = list("Gauss", "Exp", "Unif")),
-  sliderInput("noise", "noise", min = -0.1, max = 10, value = 0.1),
-  sliderInput("sigma_1", "sigma_1", min = 0.01, max = 3, value = 0.1),
-  sliderInput("sigma_2", "sigma_2", min = 0.00001, max = 1, value = 0.1),
-  sliderInput("l", "l", min = 0.1, max = 3, value = 0.1),
-  checkboxInput("drawrand", "Draw training points at random"),
-  sliderInput("xlim", "xlim", min = -10, max = 10, value = c(-5,5)),
-  plotOutput("plot1")
+  fluidRow(
+    column(width = 6,
+      sliderInput("noise", "noise", min = 0, max = 10, value = 0.1),
+      sliderInput("gennoise", "gennoise", min = 0, max = 1, value = 0.1),
+      numericInput("n", "number of training points", min = 1, value = 10),
+      checkboxInput("drawrand", "Choose training points at random"),
+      sliderInput("xlim", "xlim", min = -10, max = 10, value = c(-5,5))
+      ),
+    
+      
+    column(width = 6,
+      selectInput("cov", "Covariance Function", choices = list("Squared Exponential", "Gamma Exponential", 
+                                                                    "Constant", "Linear","Polynomial", 
+                                                                    "Rational Quadratic")),
+      uiOutput("selectors")
+      )
+  ),
+  fluidRow(
+    column(width = 12,  
+      plotOutput("plot1")))
 )
 server <- function(input, output){
+  observeEvent(input$cov, {
+    if (input$cov == "Squared Exponential"){
+      removeUI(selector = "div#selctdiv5")
+      removeUI(selector = "div#selctdiv2")
+      removeUI(selector = "div#selctdiv3")
+      removeUI(selector = "div#selctdiv4")
+      removeUI(selector = "div#selctdiv6")
+      output$selectors <- renderUI({
+        tags$div(id = "selctdiv1",
+                 withMathJax("$$\\huge{\\sigma_1 \\cdot \\text{exp} \\left( \\frac{|x_p-x_q|^2}{2 \\ell^2} \\right) + \\sigma_2 \\delta_{pq}}$$"),
+                 sliderInput("sigma_1", withMathJax("$$\\huge{\\sigma_1}$$"), min = 0.01, max = 3, value = 1),
+                 sliderInput("sigma_2", withMathJax("$$\\huge{\\sigma_2}$$"), min = 0.00001, max = 1, value = 0.1),
+                 sliderInput("l", withMathJax("$$\\huge{\\ell}$$"), min = 0.1, max = 3, value = 1)
+                  
+        )
+      })
+    }
+    if (input$cov == "Constant"){
+      removeUI(selector = "div#selctdiv1")
+      removeUI(selector = "div#selctdiv5")
+      removeUI(selector = "div#selctdiv3")
+      removeUI(selector = "div#selctdiv4")
+      removeUI(selector = "div#selctdiv6")
+      output$selectors <- renderUI({
+        tags$div(id = "selctdiv2", 
+                 withMathJax("c"),
+                 sliderInput("c", "c", min = 0, max = 10, value = 1))
+      })
+    }
+    if (input$cov == "Linear"){
+      removeUI(selector = "div#selctdiv1")
+      removeUI(selector = "div#selctdiv2")
+      removeUI(selector = "div#selctdiv5")
+      removeUI(selector = "div#selctdiv4")
+      removeUI(selector = "div#selctdiv6")
+      output$selectors <- renderUI({
+        tags$div(id = "selctdiv3", 
+                 withMathJax("$$\\huge{c \\cdot \\sum_{d=1}^D x_d x_d^\\top}$$"),
+                 sliderInput("c", "c", min = 0, max = 10, value = 1))
+      })
+    }
+    if (input$cov == "Polynomial"){
+      removeUI(selector = "div#selctdiv1")
+      removeUI(selector = "div#selctdiv2")
+      removeUI(selector = "div#selctdiv3")
+      removeUI(selector = "div#selctdiv5")
+      removeUI(selector = "div#selctdiv6")
+      output$selectors <- renderUI({
+        tags$div(id = "selctdiv4", 
+                 withMathJax("$$\\huge{(x \\cdot x'^\\top + \\sigma)^p}$$"),
+                 sliderInput("sigma_3", withMathJax("$$\\huge{\\sigma}$$"), min = 0, max = 10, value = 1),
+                 sliderInput("p", "p", min = 1, max = 10, value = 1))
+      })
+    }
+    if (input$cov == "Gamma Exponential"){
+      removeUI(selector = "div#selctdiv1")
+      removeUI(selector = "div#selctdiv2")
+      removeUI(selector = "div#selctdiv3")
+      removeUI(selector = "div#selctdiv4")
+      removeUI(selector = "div#selctdiv6")
+      output$selectors <- renderUI({
+        tags$div(id = "selctdiv5", 
+                 withMathJax("$$\\huge{\\text{exp} (- \\frac{|x-x'|}{\\ell})^\\gamma}$$"),
+                 sliderInput("sigma_4", withMathJax("$$\\huge{\\gamma}$$"), min = 0, max = 10, value = 1),
+                 sliderInput("l2", withMathJax("$$\\huge{\\ell}$$"), min = 1, max = 10, value = 1))
+      })
+    }
+    if (input$cov == "Rational Quadratic"){
+      removeUI(selector = "div#selctdiv1")
+      removeUI(selector = "div#selctdiv2")
+      removeUI(selector = "div#selctdiv3")
+      removeUI(selector = "div#selctdiv4")
+      removeUI(selector = "div#selctdiv5")
+      output$selectors <- renderUI({
+        tags$div(id = "selctdiv6", 
+                 sliderInput("alpha", "alpha", min = 0, max = 10, value = 1),
+                 sliderInput("l3", "l", min = 1, max = 10, value = 1))
+      })
+    }
+  })
   output$plot1 <- renderPlot({
-    n <- 5 #number of training points in the intervall
     if (input$drawrand){
-      X <- reactive(matrix(runif(n,input$xlim[1],input$xlim[2]), nrow = 1))
+      X <- reactive(matrix(runif(input$n,input$xlim[1],input$xlim[2]), nrow = 1))
     }
     else{
-      X <- reactive(matrix(seq(input$xlim[1],input$xlim[2],by = (input$xlim[2] - input$xlim[1])/n), nrow = 1))
+      X <- reactive(matrix(seq(input$xlim[1],input$xlim[2],
+                               by = (input$xlim[2] - input$xlim[1])/input$n), nrow = 1))
     }
-    y <- reactive(c(sin(X()) + rnorm(length(X()),0,sqrt(input$noise))))
-    #hyperparameters
-    sigma_1 <- 1
-    sigma_2 <- 0.1
-    l <- 1
-    kappa <- function(x,y){
-      input$sigma_1 * exp(-(1/(2*input$l^2))*(x - y)^2) + input$sigma_2 * ifelse(isTRUE(all.equal(x,y)),1,0)
+    y <- reactive(c(sin(X()) + rnorm(length(X()),0,sqrt(input$gennoise))))
+    Gaussian <- reactive(GPR.sqrexp$new(X(), y(), l = 1, noise = 0.1))
+    if (input$cov == "Squared Exponential"){
+      if (!is.null(input$sigma_1) & is.null(input$sigma_2) & !is.null(input$l)){
+        kappa <- reactive(function(x,y){
+        input$sigma_1 * exp(-(1/(2*input$l^2))*(x - y)^2) + input$sigma_2 * ifelse(isTRUE(all.equal(x,y)),1,0)
+        })
+        Gaussian <- reactive(GPR$new(X(), y(), kappa(), input$noise))
+      }
     }
-    Gaussian <- reactive(GPR$new(X(), y(), kappa, input$noise))
-    Gaussian()$plot(seq(input$xlim[1],input$xlim[2], by = 0.1))
+    if (input$cov == "Constant"){
+      if (!is.null(input$c)) Gaussian <- reactive(GPR.constant$new(X(), y(), input$c, input$noise))
+    }
+    if (input$cov == "Linear"){
+      if (!is.null(input$c)) Gaussian <- reactive(GPR.linear$new(X(), y(), input$c, input$noise))
+    }
+    if (input$cov == "Polynomial"){
+      if (!is.null(input$sigma_3) & !is.null(input$p)) Gaussian <- reactive(GPR.polynomial$new(X(), y(), input$sigma_3, input$p, input$noise))
+    }
+    if (input$cov == "Gamma Exponential"){
+      if (!is.null(input$sigma_4) & !is.null(input$l2)){
+      Gaussian <- reactive(GPR.gammaexp$new(X(), y(), input$sigma_4, input$l2, input$noise))
+      }
+    }
+    if (input$cov == "Rational Quadratic"){
+      if (!is.null(input$alpha) & !is.null(input$l3)){
+      Gaussian <- reactive(GPR.rationalquadratic$new(X(), y(), input$alpha, input$l3, input$noise))
+    }
+    }
+    Gaussian()$plot(seq(input$xlim[1] - 1, input$xlim[2] + 1, by = 0.1))
   })
 }
 
 shinyApp(ui, server)
+
