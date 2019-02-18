9b90ca3152ee1aca117adbecd542e272d82ec30b
MBudjan <budjan@stud.uni-heidelberg.de>
2019-02-18 17:12:35 +0100

    corrected changing variance in plot app, added sentences to vignette

--- R/GPplot.R
+++ R/GPplot.R
@@ -15,9 +15,9 @@ ui <- fluidPage(
     column(width = 6, wellPanel(
       h4("Datapoints"),
       textInput("func", "Data function of x", value = "sin(x)"),
-      sliderInput("noise", "noise", min = 0, max = 1, value = 0),
-      sliderInput("gennoise", "generating noise", min = 0, max = 1, value = 0),
-      numericInput("n", "number of training points", min = 1, value = 10),
+      sliderInput("noise", "Noise", min = 0, max = 1, value = 0),
+      sliderInput("gennoise", "Generating noise", min = 0, max = 1, value = 0),
+      numericInput("n", "Number of training points", min = 1, value = 10),
       checkboxInput("drawrand", "Choose training points at random"),
       sliderInput("xlim", "Data Boundaries", min = -10, max = 10, value = c(-5, 5))
       )),
@@ -35,15 +35,16 @@ ui <- fluidPage(
 )
 
 #Helper function to remove sliders
-switchrenderUI <- function(i, session, min_noise, kdesc, ...){
+switchrenderUI <- function(i, session, min_noise, act_noise, kdesc, ...){
   n <- nrow(cov_df) #number of options
   for (j in (1:n)[-i]) removeUI(selector = paste0("div#selectdiv", j)) #remove other UIs
   #change minimum value for noise
-  updateSliderInput(session, "noise", value = min_noise,
-                    min = min_noise)
+  updateSliderInput(session, "noise", min = min_noise)
+  if (act_noise == 0) updateSliderInput(session, "noise", value = min_noise,
+                                        min = min_noise)
   #return new UI
   renderUI({
-    tags$div(id = paste0("selctdiv", i),withMathJax(paste("$$\\huge{", kdesc, "}$$")), ...)
+    tags$div(id = paste0("selctdiv", i), withMathJax(paste("$$\\huge{", kdesc, "}$$")), ...)
   })
 }
 
@@ -59,33 +60,33 @@ server <- function(input, output, session){
     #switch formula and sliders for parameters for each covariance function
     switch(input$cov, 
     "Squared Exponential" = {
-      output$selectors <- switchrenderUI(1, session, 0,
+      output$selectors <- switchrenderUI(1, session, 0, input$noise,
         "\\text{exp} \\left(- \\frac{||x-x'||^2}{2 \\ell^2} \\right)",
-        sliderInput("par1", withMathJax("$$\\huge{\\ell}$$"), min = 0.01, max = 3, value = 1))
+        sliderInput("par1", withMathJax("$$\\huge{\\ell}$$"), min = 0.01, max = 10, value = 1))
     },
     "Constant" = {
-      output$selectors <- switchrenderUI(2, session, 0.1, "c",
+      output$selectors <- switchrenderUI(2, session, 0.1, input$noise, "c",
                      sliderInput("par1", "c", min = 0.1, max = 10, value = 1))
     },
     "Linear" = {
-      output$selectors <- switchrenderUI(3, session, 0.1,
+      output$selectors <- switchrenderUI(3, session, 0.1, input$noise,
                  "c \\cdot \\sum_{d=1}^D x_d x_d'",
                  sliderInput("par1", "c", min = 0, max = 10, value = 1.2, step = 0.1))
     },
     "Polynomial" = {
-      output$selectors <- switchrenderUI(4, session, 0.1, 
+      output$selectors <- switchrenderUI(4, session, 0.1, input$noise,
                  "(x \\cdot x'^\\top + \\sigma)^p",
                  sliderInput("par1", withMathJax("$$\\huge{\\sigma}$$"), min = 0, max = 10, value = 1, step = 0.01),
                  sliderInput("par2", withMathJax("$$\\huge{p}$$"), min = 1, max = 10, value = 5))
     },
     "Gamma Exponential" = {
-      output$selectors <- switchrenderUI(5, session, 0, 
+      output$selectors <- switchrenderUI(5, session, 0, input$noise,
                  "\\exp \\left( - \\left(\\frac{||x-x'||}{\\ell}\\right)^\\gamma \\right)",
                  sliderInput("par1", withMathJax("$$\\huge{\\gamma}$$"), min = 1, max = 10, value = 2),
                  sliderInput("par2", withMathJax("$$\\huge{\\ell}$$"), min = 0.01, max = 3, value = 1))
     },
     "Rational Quadratic" = {
-      output$selectors <- switchrenderUI(6, session, 0, 
+      output$selectors <- switchrenderUI(6, session, 0, input$noise,
                  "\\left( 1 + \\frac{||x-x'||^2}{2 \\alpha \\ell^2}\\right)^{-\\alpha}",
                  sliderInput("par1", withMathJax("$$\\huge{\\alpha}$$"), min = 0.1, max = 3, value = 1, step = 0.1),
                  sliderInput("par2", withMathJax("$$\\huge{\\ell}$$"), min = 0.01, max = 7, value = 1))
@@ -116,7 +117,6 @@ server <- function(input, output, session){
         Gaussian <- reactive(GPR.sqrexp$new(X(), y(), input$par1, input$noise))
       },
      "Constant" = {validate(need(input$par1, "Invalid parameters") %then% 
-                              need(input$noise > 0, "Invalid parameters") %then% 
                               need(input$par1 > 0, "Invalid parameters") )
         Gaussian <- reactive(GPR.constant$new(X(), y(), input$par1, input$noise))
       },
