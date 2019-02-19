23579f185d2c1a2db59eca9c9564d7aa82dd0205
moritz-ha <Moritz.Haas@stud.uni-heidelberg.de>
2019-02-19 12:22:40 +0100

    gpc tests done

--- R/GPCclass.R
+++ R/GPCclass.R
@@ -195,37 +195,3 @@ GPC <- R6::R6Class("GPC",
 f <- function(x) (sum(abs(x)) > 2.5) - (!(sum(x) > 2.5))
 limits <- matrix(c(-4, 4, -4, 4), nrow = 2, byrow = TRUE)
 k <- function(x, y) sqrexp(x, y, 1)
-#simulate_classification(func = f, limits = limits, k = k, num_data = 20)
-
-X <- matrix(seq(-1,1,by = 0.1), nrow = 1)
-y <- 2*as.integer(X > 0) - 1
-kappa <- function(x,y) exp(-3*(x - y)^2)
-gaussian_classifier <- GPC$new(X, y, 1e-5, kappa)
-gaussian_classifier$plot(seq(-2,2, by = 0.1))$plot
-
-X <- matrix(c(seq(-1, -0.1, by = 0.1), seq(0, 1, by = 0.2)), nrow = 1)
-y <- 2*as.integer(X > 0) - 1
-kappa <- function(x,y) exp(-3*(x - y)^2)
-gaussian_classifier <- GPC$new(X, y, 1e-5, kappa)
-gaussian_classifier$plot(seq(-2,2, by = 0.1))$plot
-
-s <- seq(-1, 1, by = 0.5)
-X <- matrix(c(rep(s, each = length(s)), rep(s, times = length(s))), nrow = 2, byrow = T)
-y <- 2*as.integer(X[1, ] > X[2, ]) - 1
-kappa <- function(x,y) sqrexp(x,y,l=1)
-gaussian_classifier <- GPC$new(X, y, 1e-5, kappa)
-s <- seq(-1, 1, by = 0.1)
-testpoints <- matrix(c(rep(s, each = length(s)), rep(s, times = length(s))), nrow = 2, byrow = T)
-gaussian_classifier$plot(testpoints)$plot
-
-n <- 10
-X <- cbind(multivariate_normal(n, c(0.5,0.5), diag(c(0.1,0.1))), multivariate_normal(n, c(-0.5,-0.5), diag(c(0.1,0.1))))
-plot(X[1,], X[2, ])
-y <- rep(c(1,-1), each = n)
-kappa <- function(x,y) sqrexp(x,y,l=1)
-gaussian_classifier <- GPC$new(X, y, 1e-5, kappa)
-s <- seq(-1, 1, by = 0.1)
-testpoints <- matrix(c(rep(s, each = length(s)), rep(s, times = length(s))), nrow = 2, byrow = T)
-gaussian_classifier$plot(testpoints)$plot
-
-
